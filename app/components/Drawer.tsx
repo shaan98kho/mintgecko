961dae3338e280e0bf7ef6ed9e2aedd103294930
Wen Qi Kho <shaankho1998@gmail.com>
2026-07-11 18:21:57 +0800

    feat: finally added mobile view lol

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -1,17 +1,22 @@
 import NavBtn from "./NavBtn"
 import { href, Link } from "react-router"
 import Logo from "./Logo"
-import { BiSolidDashboard } from "react-icons/bi";
-import { LuChartLine } from "react-icons/lu";
-import { FaWallet } from "react-icons/fa6";
-import { IoNewspaper } from "react-icons/io5";
-import { RiExchangeFill } from "react-icons/ri";
-import { IoIosSettings } from "react-icons/io";
+import { BiSolidDashboard } from "react-icons/bi"
+import { LuChartLine } from "react-icons/lu"
+import { FaWallet } from "react-icons/fa6"
+import { IoNewspaper } from "react-icons/io5"
+import { RiExchangeFill } from "react-icons/ri"
+import { IoIosSettings } from "react-icons/io"
+import { IoClose } from "react-icons/io5"
 
+type DrawerProps = {
+    isOn: boolean,
+    toggle: (e: React.MouseEvent<HTMLButtonElement>) => void
+}
 
-export default function Drawer() {
+export default function Drawer({isOn, toggle}: DrawerProps) {
 
-    return <div className="drawer fixed w-[240px] flex flex-col items-start justify-start min-h-screen overflow-y-auto no-scrollbar pl-4 pr-8 z-1">
+    return <div className={`drawer fixed  flex flex-col justify-start min-h-screen overflow-y-auto no-scrollbar pl-4 pr-8 z-2 ${isOn ? "" : "slideOut"}`}>
         <Link to={"/"}><Logo classes={"text-5xl pb-4 pt-6 px-2 cursor-pointer"} /></Link>
         <div className="pl-4 pt-2 pb-4 text-sm cursor-default">Overview</div>
         <div className="flex flex-col gap-4 font-medium w-full">
@@ -24,5 +29,6 @@ export default function Drawer() {
         <div className="flex flex-col gap-4 pt-8 mt-auto mb-8 w-full">
             <NavBtn caption={<><IoIosSettings/>Settings</>} href="/settings"/>
         </div>
+        <button onClick={toggle} className=" absolute size-8 right-8 top-8 cursor-pointer"><IoClose className="nav-close size-full"/></button>
     </div>
 }
--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,13 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { useState } from "react"
+import { RxHamburgerMenu } from "react-icons/rx"
+
+
 import type { Route } from "./+types/root";
 import Drawer from "~/components/Drawer";
+import Logo from "~/components/Logo"
 import "./app.css";
 
 import { Provider } from "react-redux";
@@ -46,10 +51,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+
+  function useToggle(init = false) {
+      const [on, setOn] = useState(init)
+      const toggle = () => {
+        console.log("???")
+        setOn(prev => !prev)
+      }
+      const off = () => setOn(false)
+      return {on, toggle, off}
+  }
+
+  const mainNav = useToggle()
+  
   return <Provider store={store}>
-    <div className="flex items-start w-full">
-      <Drawer />
-      <div className="min-h-screen relative w-[calc(100%-240px)] left-[240px]">
+    <div className="flex items-start w-full ">
+      <div className="main-nav-header fixed w-full z-2">
+        <Logo classes="w-fit mx-auto"/>
+        <RxHamburgerMenu className={`size-8 absolute right-4 nav-burger cursor-pointer z-1 top-2`} onClick={mainNav.toggle}/>
+      </div>
+      <Drawer isOn={mainNav.on} toggle={mainNav.toggle}/>
+      <div className="container min-h-screen relative">
         <Outlet />
       </div>
     </div>
